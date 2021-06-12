38ac343a9d47d2f2502892e1df39d0a85646e257
Teoh Kheng Hong <tkhenghong@gmail.com>
2021-06-12 16:30:14 +0800

    Changed back to initial values due to backend connected to Homebrew MongoDB instead of Docker MongoDB

--- server.js
+++ server.js
@@ -22,8 +22,7 @@ app.get('/profile-picture', function (req, res) {
 
 // use when starting application locally
 
-// let mongoUrlLocal = "mongodb://admin:password@localhost:27017"; // PROBLEM: Unable to connect to MongoDB correctly. After removing admin:password, then it can connect properly. Guess MONGO_INITDB_ROOT_USERNAME is not the username and password when using Mongo client
-let mongoUrlLocal = "mongodb://localhost:27017";
+let mongoUrlLocal = "mongodb://admin:password@localhost:27017";
 
 // use when starting application as docker container
 let mongoUrlDocker = "mongodb://admin:password@mongodb";
@@ -32,7 +31,7 @@ let mongoUrlDocker = "mongodb://admin:password@mongodb";
 let mongoClientOptions = {useNewUrlParser: true, useUnifiedTopology: true};
 
 // "user-account" in demo with docker. "my-db" in demo with docker-compose
-let databaseName = "my-db";
+let databaseName = "user-account";
 
 app.post('/update-profile', function (req, res) {
     let userObj = req.body;
