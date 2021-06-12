6e24191a338f10f9fb359d56be3d6e168a15e400
Teoh Kheng Hong <tkhenghong@gmail.com>
2021-06-12 16:01:25 +0800

    Managed to start up the server, connect to MongoDB without authentication, has comment

--- server.js
+++ server.js
@@ -6,75 +6,77 @@ let bodyParser = require('body-parser');
 let app = express();
 
 app.use(bodyParser.urlencoded({
-  extended: true
+    extended: true
 }));
 app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, "index.html"));
-  });
+});
 
 app.get('/profile-picture', function (req, res) {
-  let img = fs.readFileSync(path.join(__dirname, "images/profile-1.jpg"));
-  res.writeHead(200, {'Content-Type': 'image/jpg' });
-  res.end(img, 'binary');
+    let img = fs.readFileSync(path.join(__dirname, "images/profile-1.jpg"));
+    res.writeHead(200, {'Content-Type': 'image/jpg'});
+    res.end(img, 'binary');
 });
 
 // use when starting application locally
-let mongoUrlLocal = "mongodb://admin:password@localhost:27017";
+
+// let mongoUrlLocal = "mongodb://admin:password@localhost:27017"; // PROBLEM: Unable to connect to MongoDB correctly. After removing admin:password, then it can connect properly. Guess MONGO_INITDB_ROOT_USERNAME is not the username and password when using Mongo client
+let mongoUrlLocal = "mongodb://localhost:27017";
 
 // use when starting application as docker container
 let mongoUrlDocker = "mongodb://admin:password@mongodb";
 
 // pass these options to mongo client connect request to avoid DeprecationWarning for current Server Discovery and Monitoring engine
-let mongoClientOptions = { useNewUrlParser: true, useUnifiedTopology: true };
+let mongoClientOptions = {useNewUrlParser: true, useUnifiedTopology: true};
 
 // "user-account" in demo with docker. "my-db" in demo with docker-compose
 let databaseName = "my-db";
 
 app.post('/update-profile', function (req, res) {
-  let userObj = req.body;
+    let userObj = req.body;
 
-  MongoClient.connect(mongoUrlLocal, mongoClientOptions, function (err, client) {
-    if (err) throw err;
+    MongoClient.connect(mongoUrlLocal, mongoClientOptions, function (err, client) {
+        if (err) throw err;
 
-    let db = client.db(databaseName);
-    userObj['userid'] = 1;
+        let db = client.db(databaseName);
+        userObj['userid'] = 1;
 
-    let myquery = { userid: 1 };
-    let newvalues = { $set: userObj };
+        let myquery = {userid: 1};
+        let newvalues = {$set: userObj};
 
-    db.collection("users").updateOne(myquery, newvalues, {upsert: true}, function(err, res) {
-      if (err) throw err;
-      client.close();
+        db.collection("users").updateOne(myquery, newvalues, {upsert: true}, function (err, res) {
+            if (err) throw err;
+            client.close();
+        });
+
     });
-
-  });
-  // Send response
-  res.send(userObj);
+    // Send response
+    res.send(userObj);
 });
 
 app.get('/get-profile', function (req, res) {
-  let response = {};
-  // Connect to the db
-  MongoClient.connect(mongoUrlLocal, mongoClientOptions, function (err, client) {
-    if (err) throw err;
+    let response = {};
+    // Connect to the db
+    MongoClient.connect(mongoUrlLocal, mongoClientOptions, function (err, client) {
+        if (err) throw err;
 
-    let db = client.db(databaseName);
+        let db = client.db(databaseName);
 
-    let myquery = { userid: 1 };
+        let myquery = {userid: 1};
 
-    db.collection("users").findOne(myquery, function (err, result) {
-      if (err) throw err;
-      response = result;
-      client.close();
+        db.collection("users").findOne(myquery, function (err, result) {
+            if (err) throw err;
+            response = result;
+            client.close();
 
-      // Send response
-      res.send(response ? response : {});
+            // Send response
+            res.send(response ? response : {});
+        });
     });
-  });
 });
 
 app.listen(3000, function () {
-  console.log("app listening on port 3000!");
+    console.log("app listening on port 3000!");
 });
